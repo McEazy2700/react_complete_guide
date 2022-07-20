f2493e33fa0f137543b26bf8feafea850a25ed50
Ezekiel Victor <102771161+McEazy2700@users.noreply.github.com>
2022-07-20 08:26:04 +0100

    Created Chart and ChartBar components

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ChartBar from './ChartBar'
+import './Chart.scss'
+
+const Chart = (props) => {
+  return (
+    <div className='chart'>
+        {props.dataPoints.map(dataPoint => <ChartBar 
+        value={dataPoint.value} 
+        maxValue={null} 
+        label={dataPoint.label}
+        key={dataPoint.label}
+        />)}
+    </div>
+  )
+}
+
+export default Chart
